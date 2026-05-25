49247199f668db484994480961e8f4265cc40970
Mixta Africa <o.olasunkanmi@mixtafrica.com>
2026-05-25 12:02:48 +0100

    Update dashboard-auth.js

--- dashboard-auth.js
+++ dashboard-auth.js
@@ -51,7 +51,8 @@
     "t.banjo@mixtafrica.com":                 "Both",
     "t.ibidokun@mixtafrica.com":              "Both",
     "u.ojembe@mixtafrica.com":                "Both",
-    "w.salami@mixtafrica.com":                "Both",.
+    "w.salami@mixtafrica.com":                "Both",
+    "m.apuh@mixtafrica.com":                  "Both",
     // Lakowe Crossings only
     "b.ajayi@mixtafrica.com":                 "Crossings",
     "o.isabu@mixtafrica.com":                 "Crossings",
